2c0aaf5695ef915aa1989eb97515e8e51d76326d
Alfred Victoria <aetv9302@gmail.com>
2025-06-08 15:35:21 +0800

    refactor: screen.title to screen.name

--- setting_up_your_os_emulator/main.cpp
+++ setting_up_your_os_emulator/main.cpp
@@ -42,23 +42,22 @@ struct Screen
     int currentLine;
     int totalLines;
     std::string createdDate;
-    std::string title;
+    std::string name;
 };
 
-
-Screen createScreen(std::string title){
+Screen createScreen(std::string name){
     Screen newScreen;
     newScreen.cpuId = 0;
     newScreen.currentLine = 0;
     newScreen.totalLines = getRand(1,1000);
     newScreen.createdDate = getCurrentDateTime(); 
-    newScreen.title = title;
+    newScreen.name = name;
     return newScreen;
 }
 
 void printScreen(const Screen& screen)
 {
-    std::cout << "Screen Title: " << screen.title << "\n";
+    std::cout << "Screen Title: " << screen.name << "\n";
     std::cout << "Current Line: " << screen.currentLine << "/" << screen.totalLines << "\n";
     std::cout << "Created Date: " << screen.createdDate << "\n";
 }
@@ -84,7 +83,6 @@ void clearScreen()
     #endif
 }
 
-
 int main()
 {
     printHeader();
@@ -93,7 +91,7 @@ int main()
     std::vector<Screen> screens;
     Screen currentScreen;
     Screen mainMenu;
-    mainMenu.title = "Main Menu";
+    mainMenu.name = "Main Menu";
     currentScreen = mainMenu;
     while (true)
     {
@@ -116,7 +114,7 @@ int main()
         }
         else if (command[0] == "exit")
         {
-            if(currentScreen.title != "Main Menu"){
+            if(currentScreen.name != "Main Menu"){
                 currentScreen = mainMenu;
                 clearScreen();
                 printHeader();
@@ -126,11 +124,11 @@ int main()
                 break;
             }
         }
-        else if (command[0] == "clear" && currentScreen.title == "Main Menu") { 
+        else if (command[0] == "clear" && currentScreen.name == "Main Menu") { 
             clearScreen();
             printHeader();
         }
-        else if (command[0] == "screen" && (command.size() == 3 || command.size() == 2) && currentScreen.title == "Main Menu")
+        else if (command[0] == "screen" && (command.size() == 3 || command.size() == 2) && currentScreen.name == "Main Menu")
         {
             if(command[1] == "-s"){ // Create a new screen
                 Screen newScreen = createScreen(command[2]);
@@ -143,7 +141,7 @@ int main()
             else if(command[1] == "-r"){ // Resume existing screen
                 bool found = false;
                 for(int i = 0; i < screens.size(); i++){
-                    if(screens[i].title == command[2]){
+                    if(screens[i].name == command[2]){
                         currentScreen = screens[i];
                         clearScreen();
                         printScreen(screens[i]);
@@ -173,6 +171,10 @@ int main()
 
         }
         else if(command[0] == "print") { // GROUP HOMEWORK W6;  REMOVE/DISABLE AFTER SUBMISSION
+            // Logic: 
+            // screen -s | 10 times to generate processes and fill vector<screens>
+            // for each process in vector, make text file
+            
             for (int i = 0; i < screens.size(); i++)
             {
                 std::string filename = screens.at(i).name + ".txt";
